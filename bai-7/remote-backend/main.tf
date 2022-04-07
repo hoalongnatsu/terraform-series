d3934d62a945cbb105f5173c14fee80c938afea2
hoalongnatsu <hoalongnatsu@gmail.com>
2022-04-07 13:44:16 +0700

    bai 7

---
+++ bai-7/remote-backend/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  cloud {
+    organization = "<organization-name>"
+
+    workspaces {
+      name = "terraform-series-remote-backend"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+data "aws_ami" "ami" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"]
+}
+
+resource "aws_instance" "server" {
+  ami           = data.aws_ami.ami.id
+  instance_type = "t3.micro"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    Name = "Server"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.server.public_ip
+}
